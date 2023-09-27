ab6c50ef689a5d1895e265fa2630297dda7ec9e7
Alvin-Karanja <alvin2@proton.me>
2023-09-27 18:31:24 +0300

    initial commit

---
+++ .next/static/chunks/app_conference_styles_css.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app_conference_styles_css"],{
+
+/***/ "./app/conference/styles.css":
+/*!***********************************!*\
+  !*** ./app/conference/styles.css ***!
+  \***********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"0a686561646572207b0a202077696474683a20313030253b0a202070616464696e673a2035307078203070782035307078203b0a20206261636b67726f756e643a20233231323132313b0a2020636f6c6f723a20236666663b0a2020746578742d616c69676e3a2063656e7465723b0a20206a7573746966792d636f6e74656e743a2063656e7465723b0a7d0a\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9hcHAvY29uZmVyZW5jZS9zdHlsZXMuY3NzLmpzIiwibWFwcGluZ3MiOiI7QUFBQSwrREFBZSIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvLi9hcHAvY29uZmVyZW5jZS9zdHlsZXMuY3NzPzk4ODgiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCIwYTY4NjU2MTY0NjU3MjIwN2IwYTIwMjA3NzY5NjQ3NDY4M2EyMDMxMzAzMDI1M2IwYTIwMjA3MDYxNjQ2NDY5NmU2NzNhMjAzNTMwNzA3ODIwMzA3MDc4MjAzNTMwNzA3ODIwM2IwYTIwMjA2MjYxNjM2YjY3NzI2Zjc1NmU2NDNhMjAyMzMyMzEzMjMxMzIzMTNiMGEyMDIwNjM2ZjZjNmY3MjNhMjAyMzY2NjY2NjNiMGEyMDIwNzQ2NTc4NzQyZDYxNmM2OTY3NmUzYTIwNjM2NTZlNzQ2NTcyM2IwYTIwMjA2YTc1NzM3NDY5NjY3OTJkNjM2ZjZlNzQ2NTZlNzQzYTIwNjM2NTZlNzQ2NTcyM2IwYTdkMGFcIiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./app/conference/styles.css\n"));
+
+/***/ })
+
+}]);
